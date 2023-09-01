e34b945dedac88c2e9fb302bc38ffe5f5ffb6ac2
Ata Tekeli <tekeliata2000@gmail.com>
2023-09-01 19:10:47 +0000

    1 Eyl 2023 22:10

--- template.py
+++ template.py
@@ -32,3 +32,20 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    if(not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filename}")
+    
+    else:
+        logging.info(f"{filename} is already created")
