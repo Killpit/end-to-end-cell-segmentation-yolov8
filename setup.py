130751a5c11b47c32de420f19c6bb0a0a27272a6
Ata Tekeli <tekeliata2000@gmail.com>
2023-09-01 21:57:12 +0000

    2 Eyl 2023 00:57

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version = '0.0.0',
+    author = 'Ata Tekeli',
+    author_email = 'tekeliata2000@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
