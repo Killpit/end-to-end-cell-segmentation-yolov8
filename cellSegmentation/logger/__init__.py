0a4b5dba27a3e650fd93eac42d0c223b269b9a6d
Ata Tekeli <tekeliata2000@gmail.com>
2023-09-02 13:48:18 +0000

    2 Eyl 2023 16:48

--- cellSegmentation/logger/__init__.py
+++ cellSegmentation/logger/__init__.py
@@ -4,3 +4,9 @@ from datetime import datetime
 from from_root import from_root
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_path = os.path.join(from_root(), 'log', LOG_FILE)
+
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
