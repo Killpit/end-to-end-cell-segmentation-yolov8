2129f5f08e1809e7647123ba600138a8ed27877b
Ata Tekeli <tekeliata2000@gmail.com>
2023-09-24 16:25:48 +0300

    Completed Colab phase, worked with pipeline and components

--- cellSegmentation/constant/training_pipeline/__init__.py
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -10,3 +10,12 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1NcQwtAfofBkRwA1bNo8w6JgIxYB6_zis/view?usp=sharing"
 
+"""
+Data validation related constant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
